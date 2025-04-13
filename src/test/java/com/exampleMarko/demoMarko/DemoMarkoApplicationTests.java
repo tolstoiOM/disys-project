af5c5bb0330b05ceeb575e74f4ceb41026e1a67e
Marko <wi23b017@technikum-wien.at>
2025-04-13 12:57:22 +0200

    initial commit

---
+++ src/test/java/com/exampleMarko/demoMarko/DemoMarkoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exampleMarko.demoMarko;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoMarkoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
